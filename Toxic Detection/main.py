7d741ece3076d2eb388cfbeb31b739177beeaf06
Evan Tanudjaja <32850711+ElectricEvan@users.noreply.github.com>
2020-11-27 12:21:31 -0500

    Set main.py as answer sheet

--- Toxic Detection/main.py
+++ Toxic Detection/main.py
@@ -3,22 +3,19 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import json
-# load the .env file
 
-
-load_dotenv()
-# setting prefix for commands
-client = commands.Bot(command_prefix = ".evan")
-TOKEN = os.getenv('DISCORD_TOKEN')
+load_dotenv(".env")
+client = commands.Bot(command_prefix=".ect")
+client.remove_command("help")
 
 # Dictionaries
 pos_words = open(r"positive_words.txt", "r")
 neg_words = open(r"negative_words.txt", "r")
 
-# creating on ready command to let us know the bot is online
+
 @client.event
 async def on_ready():
-    print("Bot is online. ")
+    print("My Boty is ready...")
 
 
 async def read_dic(msg, dic):
@@ -27,7 +24,7 @@ async def read_dic(msg, dic):
     for line in dic.readlines():
         word = line.split()[0].lower()
         if word in msg.content.lower():
-            points = int(line.split()[1])
+            points = int(line.split()[1].lower())
         else:
             points = 0
 
@@ -48,7 +45,6 @@ async def edit_user(msg, user, points):
         users[user]["Reputation"] = 0
         users[user]["Kindness"] = 0
         users[user]["Toxicness"] = 0
-
     if points > 0:
         users[user]["Positive Count"] += 1
         users[user]["Reputation"] += points
@@ -65,25 +61,26 @@ async def edit_user(msg, user, points):
             users[user]["Toxicness"] = 30
 
         await msg.channel.send(f"{msg.author.mention},"
-                               f" you should refrain from being toxic [{users[user]['Toxicness']}/30]")
-        if users[user]["Toxicness"] == 30:
+                               f" you should probably refrain from being toxic *[{users[user]['Toxicness']}/30]*")
+        if users[user]["Toxicness"] >= 30:
             try:
                 await msg.author.ban(reason="Toxic Behaviour")
-                await msg.channel.send(f"{msg.author} has been banned")
+                await msg.channel.send(f"{msg.author} has been *banned*")
             except discord.Forbidden:
-                await msg.channel.send("**Error**  My permissions are not high enough to ban this user")
+                await msg.channel.send("**ERROR:**  My permissions are not high enough to ban this user")
 
     with open("users.json", "w") as file:
         json.dump(users, file, indent=4)
 
-# reading messages sent in a discord and responds to specific phrases or words
+
 @client.event
 async def on_message(msg):
-   if not msg.author.bot:
-       max_points = await read_dic(msg, pos_words)
-       await edit_user(msg, str(msg.author.id), max_points)
+    if not msg.author.bot:
+        max_points = await read_dic(msg, pos_words)
+        await edit_user(msg, str(msg.author.id), max_points)
 
-       max_points = await read_dic(msg, neg_words)
-       await edit_user(msg, str(msg.author.id), -max_points)
+        max_points = await read_dic(msg, neg_words)
+        await edit_user(msg, str(msg.author.id), -max_points)
 
-client.run(TOKEN)
+
+client.run(os.getenv("DISCORD_TOKEN"))
