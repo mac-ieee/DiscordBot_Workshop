c443530e46b258e6b92ab5078a126d0999e03097
Evan Tanudjaja <32850711+ElectricEvan@users.noreply.github.com>
2020-11-28 13:46:48 -0500

    Added profile checking

--- Toxic Detection/main.py
+++ Toxic Detection/main.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 import json
 
 load_dotenv(".env")
-client = commands.Bot(command_prefix=".ect")
+client = commands.Bot(command_prefix="et ")
 client.remove_command("help")
 
 # Dictionaries
-pos_words = open(r"positive_words.txt", "r")
-neg_words = open(r"negative_words.txt", "r")
+pos_words = open("positive_words.txt", "r")
+neg_words = open("negative_words.txt", "r")
 
 
 @client.event
@@ -24,7 +24,7 @@ async def read_dic(msg, dic):
     for line in dic.readlines():
         word = line.split()[0].lower()
         if word in msg.content.lower():
-            points = int(line.split()[1].lower())
+            points = int(line.split()[1])
         else:
             points = 0
 
@@ -40,13 +40,10 @@ async def edit_user(msg, user, points):
 
     if user not in users:
         users[user] = {}
-        users[user]["Positive Count"] = 0
-        users[user]["Negative Count"] = 0
         users[user]["Reputation"] = 0
         users[user]["Kindness"] = 0
         users[user]["Toxicness"] = 0
     if points > 0:
-        users[user]["Positive Count"] += 1
         users[user]["Reputation"] += points
         users[user]["Kindness"] += points
 
@@ -54,7 +51,6 @@ async def edit_user(msg, user, points):
             users[user]["Kindness"] -= 24
             users[user]["Toxicness"] -= 1
     elif points < 0:
-        users[user]["Negative Count"] += 1
         users[user]["Reputation"] += points
         users[user]["Toxicness"] -= points
         if users[user]["Toxicness"] > 30:
@@ -63,6 +59,7 @@ async def edit_user(msg, user, points):
         await msg.channel.send(f"{msg.author.mention},"
                                f" you should probably refrain from being toxic *[{users[user]['Toxicness']}/30]*")
         if users[user]["Toxicness"] >= 30:
+            users[user]["Toxicness"] = 0
             try:
                 await msg.author.ban(reason="Toxic Behaviour")
                 await msg.channel.send(f"{msg.author} has been *banned*")
@@ -82,5 +79,39 @@ async def on_message(msg):
         max_points = await read_dic(msg, neg_words)
         await edit_user(msg, str(msg.author.id), -max_points)
 
+    await client.process_commands(msg)
+
+
+@client.command(aliases=["p", "prof"])
+async def profile(ctx, victim: discord.User = None):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    if victim and not victim.bot:
+        user = str(victim.id)
+        user_info = victim
+    else:
+        user = str(ctx.author.id)
+        user_info = ctx.author
+
+    if user not in users:
+        users[user] = {}
+        users[user]["Reputation"] = 0
+        users[user]["Kindness"] = 0
+        users[user]["Toxicness"] = 0
+
+        with open("users.json", "w") as file:
+            json.dump(users, file, indent=4)
+
+    profile_embed = discord.Embed(colour=0X2072AA)
+    profile_embed.set_author(name=f"{user_info.name}'s Profile", icon_url=user_info.avatar_url)
+    profile_embed.set_thumbnail(url=user_info.avatar_url)
+    profile_embed.add_field(name="STATS:",
+                            value=f"**Reputation:** {users[user]['Reputation']}\n"
+                                  f"**Kindness:** {users[user]['Kindness']}\n"
+                                  f"**Toxicness:** {users[user]['Toxicness']}",
+                            inline=False)
+
+    await ctx.send(embed=profile_embed)
 
 client.run(os.getenv("DISCORD_TOKEN"))
