ce6a4c4c3b085d571eef7e21a6e7e0df982d4022
Evan Tanudjaja <32850711+ElectricEvan@users.noreply.github.com>
2021-02-27 13:14:03 -0500

    Update main.py

--- Toxic Detection/main.py
+++ Toxic Detection/main.py
@@ -1,119 +1,49 @@
+# Imports
 import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
-import json
 
+# load the .env file
 load_dotenv(".env")
-client = commands.Bot(command_prefix="et ")
-client.remove_command("help")
+client = commands.Bot(command_prefix="MR. HAT!")
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Define Constants
+swear_ban_time = 15
+
+# Define Dictionaries
+swear_words = open("swear_words.txt", "r")
 
 
-# Dictionaries, if these return an error, uncomment the line below
-# os.chdir(r"File directory of your files e.g. C:\folder\positive_words.txt")
-pos_words = open("positive_words.txt", "r")
-neg_words = open("negative_words.txt", "r")
-
-
+# creating on ready command to let us know the bot is online
 @client.event
 async def on_ready():
     print("My Boty is ready...")
 
 
+# Read Dictionary Method
 async def read_dic(msg, dic):
-    max_points = 0
-    dic.seek(0)
-    for line in dic.readlines():
-        word = line.split()[0].lower()
-        if word in msg.content.lower():
-            points = int(line.split()[1])
-        else:
-            points = 0
+    pass
 
-        if points > max_points:
-            max_points = points
-
-    return max_points
-
-
-async def edit_user(msg, user, points):
-    with open("users.json", "r") as file:
-        users = json.load(file)
-
-    if user not in users:
-        users[user] = {}
-        users[user]["Reputation"] = 0
-        users[user]["Kindness"] = 0
-        users[user]["Toxicness"] = 0
-    if points > 0:
-        users[user]["Reputation"] += points
-        users[user]["Kindness"] += points
-
-        if users[user]["Kindness"] >= 24 and users[user]["Toxicness"] > 0:
-            users[user]["Kindness"] -= 24
-            users[user]["Toxicness"] -= 1
-    elif points < 0:
-        users[user]["Reputation"] += points
-        users[user]["Toxicness"] -= points
-        if users[user]["Toxicness"] > 30:
-            users[user]["Toxicness"] = 30
-
-        await msg.channel.send(f"{msg.author.mention},"
-                               f" you should probably refrain from being toxic *[{users[user]['Toxicness']}/30]*")
-        if users[user]["Toxicness"] >= 30:
-            users[user]["Toxicness"] = 0
-            try:
-                await msg.author.ban(reason="Toxic Behaviour")
-                await msg.channel.send(f"{msg.author} has been *banned*")
-            except discord.Forbidden:
-                await msg.channel.send("**ERROR:**  My permissions are not high enough to ban this user")
-
-    with open("users.json", "w") as file:
-        json.dump(users, file, indent=4)
-
-
+# reading messages sent in a discord and responds to specific phrases or words
 @client.event
 async def on_message(msg):
-    if not msg.author.bot:
-        max_points = await read_dic(msg, pos_words)
-        await edit_user(msg, str(msg.author.id), max_points)
+    # Swear Check: Did the person swear? Is this person a REAL person? Are we REAL?
+    if condition:
+        # Role Check: Is the bot more superior than the offending user? Is the user the server owner?
+        if condition:
+            pass
+        else:
+            pass
 
-        max_points = await read_dic(msg, neg_words)
-        await edit_user(msg, str(msg.author.id), -max_points)
-
+    # processes all commands in messages so bot can read other commands afterwards
     await client.process_commands(msg)
 
 
-@client.command(aliases=["p", "prof"])
-async def profile(ctx, victim: discord.User = None):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+# command input your prefix then your command
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
-    if victim and not victim.bot:
-        user = str(victim.id)
-        user_info = victim
-    else:
-        user = str(ctx.author.id)
-        user_info = ctx.author
-
-    if user not in users:
-        users[user] = {}
-        users[user]["Reputation"] = 0
-        users[user]["Kindness"] = 0
-        users[user]["Toxicness"] = 0
-
-        with open("users.json", "w") as file:
-            json.dump(users, file, indent=4)
-
-    profile_embed = discord.Embed(colour=0X2072AA)
-    profile_embed.set_author(name=f"{user_info.name}'s Profile", icon_url=user_info.avatar_url)
-    profile_embed.set_thumbnail(url=user_info.avatar_url)
-    profile_embed.add_field(name="STATS:",
-                            value=f"**Reputation:** {users[user]['Reputation']}\n"
-                                  f"**Kindness:** {users[user]['Kindness']}\n"
-                                  f"**Toxicness:** {users[user]['Toxicness']}",
-                            inline=False)
-
-    await ctx.send(embed=profile_embed)
-
-client.run(os.getenv("DISCORD_TOKEN"))
+client.run(TOKEN)
